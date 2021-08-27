725c8be5e66474810aba2427dcfdc6e18cc41311
Arun Subramanian R <Arun.SubramanianR@microfocus.com>
2021-08-27 17:29:42 +0530

    Basic UI App CSS

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,25 +1,18 @@
 import type { Component } from "solid-js";
+import { Routes, Route } from "solid-app-router";
 
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+import Header from "./components/Header/Header";
+import BookList from "./components/BookList/BookList";
+import BookDetail from "./components/BookDetail/BookDetail";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/details/:id" element={<BookDetail />} />
+      </Routes>
     </div>
   );
 };
