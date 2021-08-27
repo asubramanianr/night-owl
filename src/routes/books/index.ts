e3228023deaad93c32f95655056efd134aef5051
Arun Subramanian R <Arun.SubramanianR@microfocus.com>
2021-08-27 17:27:15 +0530

    Add Book get routes

--- src/routes/books/index.ts
+++ src/routes/books/index.ts
@@ -1,8 +1,12 @@
 import { FastifyPluginAsync } from "fastify";
 
-const books: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/", async function (request, reply) {
-    return { title: "this is the books api" };
+const books: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.get("/", async function () {
+    return await fastify.prisma.book.findMany();
+  });
+  fastify.get<{ Params: { id: string } }>("/:id", async function (request) {
+    const book_id = request.params.id;
+    return await fastify.prisma.book.findUnique({ where: { book_id } });
   });
 };
 
